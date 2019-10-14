4ccc9eff7ca7aa37764e45339fce6f83dbcef797
Shou C <scf@ieee.org>
2019-10-14 10:25:02 -0700

    linking new apis

--- js/sexpert.js
+++ js/sexpert.js
@@ -66,6 +66,8 @@ function toggle_modal(html) {
     modal.classList.toggle("show-modal");
     if (html){
         document.getElementById("modal_content").innerHTML = html
+    } else {
+        location.reload();
     }
 }
 
@@ -77,21 +79,24 @@ function submit_inquiry() {
         'country': get_val("country"),
         'message': get_val("message"),
         'status': 0,
-    })
+    });
+    location.reload();
 }
 
 function assign_inquiry(i, assignee_id) {
     post("/?rest_route=/sexpert/v1/assignment/" + i, {
         'assignee_id': assignee_id,
         '_wpnonce': php_variables.nonce
-    })
+    });
+    location.reload();
 }
 
 function unassign_inquiry(i, assignee_id) {
     del("/?rest_route=/sexpert/v1/assignment/" + i, {
         'assignee_id': assignee_id,
         '_wpnonce': php_variables.nonce
-    })
+    });
+    location.reload();
 }
 
 function open_inquiry_modal(i, inquirer_info, message, response) {
@@ -108,7 +113,9 @@ function open_inquiry_modal(i, inquirer_info, message, response) {
                 <div class='message'>${message}</div>
             </div>
             <br>
-            <textarea rows="10" id="response">${response === "No response yet" ? "" : response}1</textarea>
+            <textarea rows="10" 
+                id="response" 
+                onchange="save_response(${i})">${response === "No response yet" ? "" : response}</textarea>
             <br><br>
             <button class="button button-secondary" onclick="submit_response(${i})">Submit</button>
             <button class="button button-primary">Send</button>
@@ -116,10 +123,39 @@ function open_inquiry_modal(i, inquirer_info, message, response) {
     )
 }
 
+function save_response(i) {
+    patch("/?rest_route=/sexpert/v1/response_of_inquiry/" + i, {
+        'response': get_val("response"),
+        '_wpnonce': php_variables.nonce
+    })
+}
+
 
 function submit_response(i) {
     post("/?rest_route=/sexpert/v1/response_of_inquiry/" + i, {
         'response': get_val("response"),
         '_wpnonce': php_variables.nonce
-    })
+    });
+    toggle_modal();
+}
+
+function send_response(i, t) {
+    post("/?rest_route=/sexpert/v1/response_of_inquiry/" + i, {
+        'response': get_val("response"),
+        '_wpnonce': php_variables.nonce
+    });
+    if (t) {toggle_modal()}
+}
+
+function change_showing_status() {
+    let new_status = get_val("contributor_selector");
+    let re_exp = /&status=\d/;
+    if (new_status === "-1"){
+        window.location.href = window.location.href.replace(re_exp, "");
+    } else if (re_exp.exec(window.location.href)) {
+        window.location.href = window.location.href.replace(re_exp,
+            "&status=" + new_status);
+    } else {
+        window.location.href += ("&status=" + new_status);
+    }
 }
