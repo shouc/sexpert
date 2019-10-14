ecf43b19d5267e996044a3cea45e17ca1cf9fb89
Shou C <scf@ieee.org>
2019-10-14 07:54:34 -0700

    add status control

--- api/update-inquiry.php
+++ api/update-inquiry.php
@@ -1,17 +1,24 @@
 <?php
 
-function submit(WP_REST_Request $request){
+function submit(WP_REST_Request $request,
+                $need_update_status=false,
+                $new_status=0){
     global $wpdb, $INQUIRY_TABLE_NAME;
     $id = $request->get_param("id");
     $response = $request->get_param("response");
 
     // TODO: add authorization check
 
+    $new_data = array(
+        'response' => $response,
+    );
+
+    if ($need_update_status){
+        $new_data["status"] = $new_status;
+    }
     $wpdb->update(
         $INQUIRY_TABLE_NAME,
-        array(
-            'response' => $response,
-        ),
+        $new_data,
         array('id' => $id)
     );
 
@@ -23,11 +30,15 @@ function submit(WP_REST_Request $request){
     );
 }
 
+function save_response(WP_REST_Request $request){
+    submit($request, false);
+}
+
 function submit_response(WP_REST_Request $request){
-    submit($request);
+    submit($request, true, 3);
 }
 
 function send_response(WP_REST_Request $request){
-    submit($request);
+    submit($request, true, 4);
     # send_email();
 }
