13d058c12f1847584fa171ad9113bc348bd96648
Shou C <scf@ieee.org>
2019-10-16 16:27:36 -0700

    update styling of the form

--- home/form.php
+++ home/form.php
@@ -2,14 +2,54 @@
 
 function form_creation(){
     ?>
-        Email: <input type="text" id="email"><br>
-        Age: <input type="text" id="age"><br>
-        Gender: <input type="text" id="gender"><br>
-        Country: <input type="text" id="country"><br>
+    <div class="sexpert-form">
+        <table border="1">
+            <tr>
+                <th>
+                    <span>Email</span>
+                </th>
+                <th>
+                    <input class="sexpert-form-input" type="text" id="email">
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    <span>Age</span>
+                </th>
+                <th>
+                    <input class="sexpert-form-input" type="number" id="age">
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    <span>Gender</span>
+                </th>
+                <th>
+                    <select class="sexpert-form-select" id="gender">
+                        <option value="0">Trans Male</option>
+                        <option value="1">Cis Male</option>
+                        <option value="2">Trans Female</option>
+                        <option value="3">Cis Female</option>
+                        <option value="4">I don't know - Male</option>
+                        <option value="5">I don't know - Female</option>
+                        <option value="6">Don't want to disclose</option>
+                    </select>
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    <span>Country</span>
+                </th>
+                <th>
+                    <select class="sexpert-form-select" id="country"></select>
+                </th>
+            </tr>
+        </table>
 
-        Message: <textarea id="message" placeholder="Enter text here…"></textarea>
+        <span class="message">Message:</span>
+        <textarea id="message" placeholder="Enter text here…"></textarea>
         <br>
         <button onclick="submit_inquiry()">Submit</button>
-
+    </div>
     <?php
 }
