d247fb54973cdb40933d28b86d6501cdb3a72454
Shou C <scf@ieee.org>
2020-02-19 17:16:02 -0800

    add dummy fields

--- admin/index.php
+++ admin/index.php
@@ -10,13 +10,34 @@ require_once "table.php";
 function init_admin(){
     ?>
     <div class="wrap">
-    <button class="button button-primary">Generate User Report</button>
+    <p id="status"></p>
+    <button class="button button-primary" id="status_button" onclick="change_status()"></button>
+    <div style="display: flex; flex-direction: column;">
+        <p>Inquiry Received Title</p>
+        <textarea id="inquiry_received_title"></textarea>
+        <p>Inquiry Received Content</p>
+        <textarea id="inquiry_received"></textarea>
+        <p>Inquiry Resolved Title</p>
+        <textarea id="inquiry_resolved_title"></textarea>
+        <p>Inquiry Resolved Content</p>
+        <textarea id="inquiry_resolved"></textarea>
+        <p>Disabled Banner Content</p>
+        <textarea id="disabled_banner"></textarea>
+        <p>Disclaimer Content</p>
+        <textarea id="disclaimer"></textarea>
+        <br>
+        <button class="button button-primary" id="config_button" onclick="change_config()">Change config</button>
 
+    </div>
     <?php
     $exampleListTable = new Inquiry_List_Table();
     $exampleListTable->prepare_items();
     $exampleListTable->display();
     ?>
     </div>
+    <script>
+        get_status();
+        get_config();
+    </script>
     <?php
 }
